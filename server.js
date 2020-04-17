234520d05eb7912cd8442b739a5095787938dbc9
Glitch (hello-express) <none>
2020-04-17 09:26:33 +0000

    🏮🌞 Checkpoint ./server.js:3168504/4767

--- server.js
+++ server.js
@@ -6,61 +6,64 @@
 const express = require("express");
 const app = express();
 
-var randomize = require('randomatic');
-var session = require('express-session');
+var randomize = require("randomatic");
+var session = require("express-session");
 
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 
-const endb = require('endb');
+const endb = require("endb");
 var user = new endb("sqlite://user.db");
 
 app.use(express.static("public"));
 
-app.use(session({
-	secret: process.env.SECRET,
-	resave: true,
-	saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-app.get("/login", (request, response) => {
   response.sendFile(__dirname + "/views/login.html");
 });
+
+// app.get("/login", (request, response) => {
+//   response.sendFile(__dirname + "/views/login.html");
+// });
 
 app.get("/signup", (request, response) => {
   response.sendFile(__dirname + "/views/signup.html");
 });
 
 app.get("/projectname", (req, res) => {
-  let name = randomize('Aa0', 10);
+  let name = randomize("Aa0", 10);
   let data = { name: name };
   res.send(data);
 });
 
-app.post('/auth', function(request, response) {
-	var username = request.body.username;
-	var password = request.body.password;
-	if (username && password) {
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			if (results.length > 0) {
-				request.session.loggedin = true;
-				request.session.username = username;
-				response.redirect('/home');
-			} else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
-		});
-	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
-	}
+app.post("/auth", async function(request, response) {
+  var username = request.body.username;
+  var password = request.body.password;
+  if (username && password) {
+    let hasuser = await user.has(username);
+    if (hasuser) {
+      let pass = await endb.get(username);
+      if (pass === password) {
+        request.session.loggedin = true;
+        request.session.username = username;
+        response.redirect("/home");
+      } else {
+        response.send("Incorrect Username and/or Password!");
+      }
+      response.end();
+    } else {
+      response.redirect("/signup");
+    }
+  }
 });
 
 // listen for requests :)
