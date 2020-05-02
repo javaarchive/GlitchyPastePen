ed77607b00a00763807fb50de57d55ef3653cea2
Glitch (glitchypastepen) <none>
2020-05-02 06:57:40 +0000

    style: prettier the code

--- public/login.js
+++ public/login.js
@@ -11,18 +11,17 @@ const login = () => {
   })
     .then(response => response.json())
     .then(data => {
-      
       if (data.redirect == "signup") {
         window.location.href = "https://glitchypastepen.glitch.me/signup";
       } else if (data.redirect == "editor") {
         window.location.href = "https://glitchypastepen.glitch.me/me";
       } else if (data.redirect == "/") {
-        window.location.href = "https://glitchypastepen.glitch.me/redirect/loginfail";
+        window.location.href =
+          "https://glitchypastepen.glitch.me/redirect/loginfail";
       } else {
-        console.log("yo!")
+        console.log("yo!");
       }
-    
+
       let username = data.username;
-    
     });
 };
--- public/script.js
+++ public/script.js
@@ -21,4 +21,3 @@ function dismiss() {
 //   });
 
 console.log(":)");
-
